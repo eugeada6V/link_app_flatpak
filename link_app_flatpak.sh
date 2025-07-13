4ccd546a7bf5e97bba441808789a05a6b8d1663a
Ugeen65 <eugeada@tuta.com>
2025-07-13 08:16:23 +0200

    Add files via upload

---
+++ link_app_flatpak.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+##############################################################
+#
+# SU I3 FAR VEDERE LE APP FLATPAK A ROFI
+#
+# si deve linkare le app di flatpak sul percorso /usr/bin
+#
+#
+# by Eugenio Adami
+#
+# date: 13/07/2025
+#
+# licenza Gpl2
+#
+##############################################################
+
+
+# linkiamo Brave Browser
+sudo ln -s /var/lib/flatpak/exports/bin/com.brave.Browser /usr/bin/brave
+
+# linkiamo zapzap
+sudo ln -s /var/lib/flatpak/exports/bin/com.rtosta.zapzap /usr/bin/zapzap/
+
+
+
+# altre app linkiamole qua sotto:
+#################################################
+
